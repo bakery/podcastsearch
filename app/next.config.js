ef778264ec442ca1547cf31c2e84e88121d4bdfb
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2023-07-24 11:27:52 +0100

    try webpack config update

--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  webpack: (config) => {
+    // Ignore node-specific modules when bundling for the browser
+    // https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;

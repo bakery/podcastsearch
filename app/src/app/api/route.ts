fa85505de103b889cf4444c9c530ac5e44811a94
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2023-07-24 11:38:31 +0100

    test pure transformers

--- app/src/app/api/route.ts
+++ app/src/app/api/route.ts
@@ -1,41 +1,43 @@
 import { NextResponse } from "next/server";
-import "@xenova/transformers";
-import { ChromaClient, TransformersEmbeddingFunction } from "chromadb";
+import { pipeline, env } from "@xenova/transformers";
+// import "@xenova/transformers";
+// import { ChromaClient, TransformersEmbeddingFunction } from "chromadb";
 import { SearchResult } from "@/helpers";
 
-const client = new ChromaClient({
-  path: process.env.CHROMA_SERVER_URL,
-});
-const transcriptsCollectionParams = {
-  name: "transcripts",
-  embeddingFunction: new TransformersEmbeddingFunction(),
-};
+// const client = new ChromaClient({
+//   path: process.env.CHROMA_SERVER_URL,
+// });
+// const transcriptsCollectionParams = {
+//   name: "transcripts",
+//   embeddingFunction: new TransformersEmbeddingFunction(),
+// };
 
 export const runtime = "edge";
 
 export async function POST() {
-  const query = "marketing";
-  let transcripts = await client.getCollection(transcriptsCollectionParams);
-  const r = await transcripts.query({
-    queryTexts: query,
-    nResults: 10,
-  });
+  // const query = "marketing";
+  // let transcripts = await client.getCollection(transcriptsCollectionParams);
+  // const r = await transcripts.query({
+  //   queryTexts: query,
+  //   nResults: 10,
+  // });
 
-  const results: SearchResult[] = [];
+  // const results: SearchResult[] = [];
 
-  for (let i = 0; i < r.documents[0].length; i++) {
-    const d = r.documents[0][i];
-    const md = r.metadatas[0][i];
-    r.distances;
+  // for (let i = 0; i < r.documents[0].length; i++) {
+  //   const d = r.documents[0][i];
+  //   const md = r.metadatas[0][i];
+  //   r.distances;
 
-    results.push({
-      source: md?.source as string,
-      id: md?.id as string,
-      text: d,
-      start: Math.floor(md?.start as number),
-      end: Math.floor(md?.end as number),
-    });
-  }
+  //   results.push({
+  //     source: md?.source as string,
+  //     id: md?.id as string,
+  //     text: d,
+  //     start: Math.floor(md?.start as number),
+  //     end: Math.floor(md?.end as number),
+  //   });
+  // }
 
-  return NextResponse.json(results);
+  // return NextResponse.json(results);
+  return NextResponse.json({ result: typeof pipeline });
 }

4fd8a63d8807435bfda2a014306b37b6cfa502ca
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2023-07-25 09:16:47 +0100

    try original client setup

--- app/src/app/api/route.ts
+++ app/src/app/api/route.ts
@@ -1,57 +1,57 @@
 import { NextResponse } from "next/server";
 import { pipeline, env } from "@xenova/transformers";
 // import "@xenova/transformers";
-import { ChromaClient } from "chromadb";
+import { ChromaClient, TransformersEmbeddingFunction } from "chromadb";
 import { SearchResult } from "@/helpers";
 
 // @ts-ignore
 env.allowLocalModels = false;
 // export const runtime = "edge";
 
-class TransformersEmbeddingFunction {
-  private pipelinePromise: Promise<any> | null;
+// class TransformersEmbeddingFunction {
+//   private pipelinePromise: Promise<any> | null;
 
-  /**
-   * TransformersEmbeddingFunction constructor.
-   * @param options The configuration options.
-   * @param options.model The model to use to calculate embeddings. Defaults to 'Xenova/all-MiniLM-L6-v2', which is an ONNX port of `sentence-transformers/all-MiniLM-L6-v2`.
-   * @param options.revision The specific model version to use (can be a branch, tag name, or commit id). Defaults to 'main'.
-   * @param options.quantized Whether to load the 8-bit quantized version of the model. Defaults to `false`.
-   * @param options.progress_callback If specified, this function will be called during model construction, to provide the user with progress updates.
-   */
-  constructor({
-    model = "Xenova/all-MiniLM-L6-v2",
-    revision = "main",
-    quantized = false,
-    progress_callback = undefined,
-  }: {
-    model?: string;
-    revision?: string;
-    quantized?: boolean;
-    progress_callback?: Function | undefined;
-  } = {}) {
-    // Store a promise that resolves to the pipeline
-    this.pipelinePromise = new Promise(async (resolve, reject) => {
-      try {
-        resolve(
-          await pipeline("feature-extraction", model, {
-            quantized,
-            revision,
-            progress_callback,
-          })
-        );
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
+//   /**
+//    * TransformersEmbeddingFunction constructor.
+//    * @param options The configuration options.
+//    * @param options.model The model to use to calculate embeddings. Defaults to 'Xenova/all-MiniLM-L6-v2', which is an ONNX port of `sentence-transformers/all-MiniLM-L6-v2`.
+//    * @param options.revision The specific model version to use (can be a branch, tag name, or commit id). Defaults to 'main'.
+//    * @param options.quantized Whether to load the 8-bit quantized version of the model. Defaults to `false`.
+//    * @param options.progress_callback If specified, this function will be called during model construction, to provide the user with progress updates.
+//    */
+//   constructor({
+//     model = "Xenova/all-MiniLM-L6-v2",
+//     revision = "main",
+//     quantized = false,
+//     progress_callback = undefined,
+//   }: {
+//     model?: string;
+//     revision?: string;
+//     quantized?: boolean;
+//     progress_callback?: Function | undefined;
+//   } = {}) {
+//     // Store a promise that resolves to the pipeline
+//     this.pipelinePromise = new Promise(async (resolve, reject) => {
+//       try {
+//         resolve(
+//           await pipeline("feature-extraction", model, {
+//             quantized,
+//             revision,
+//             progress_callback,
+//           })
+//         );
+//       } catch (e) {
+//         reject(e);
+//       }
+//     });
+//   }
 
-  public async generate(texts: string[]): Promise<number[][]> {
-    let pipe = await this.pipelinePromise;
-    let output = await pipe(texts, { pooling: "mean", normalize: true });
-    return output.tolist();
-  }
-}
+//   public async generate(texts: string[]): Promise<number[][]> {
+//     let pipe = await this.pipelinePromise;
+//     let output = await pipe(texts, { pooling: "mean", normalize: true });
+//     return output.tolist();
+//   }
+// }
 
 const client = new ChromaClient({
   path: process.env.CHROMA_SERVER_URL,
@@ -61,8 +61,9 @@ const transcriptsCollectionParams = {
   embeddingFunction: new TransformersEmbeddingFunction(),
 };
 
-export async function POST() {
-  const query = "marketing";
+// @ts-ignore
+export async function POST(request) {
+  const { query } = await request.json();
   let transcripts = await client.getCollection(transcriptsCollectionParams);
   const r = await transcripts.query({
     queryTexts: query,
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -19,11 +19,9 @@ export default function Home() {
         <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
           <SearchBar
             onChange={async (query) => {
-              // const rs = await search(query);
-              // setResults(rs);
               const response = await fetch("/api", {
                 method: "POST",
-                body: JSON.stringify({}),
+                body: JSON.stringify({ query }),
               });
 
               setResults(await response.json());
